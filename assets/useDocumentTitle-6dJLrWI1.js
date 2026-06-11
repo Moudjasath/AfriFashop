d2a9166f054878925c3724e47c5c28b908ee0b37
InawoSuite <inawotechnologies@gmail.com>
2026-06-11 09:59:17 +0100

    Updates

--- assets/useDocumentTitle-6dJLrWI1.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,g as t}from"./index-DsyNhHa0.js";var n=e(t(),1),r=`AfriFashop`;function i(e){(0,n.useEffect)(()=>(document.title=e?`${e} — ${r}`:r,()=>{document.title=r}),[e])}export{i as t};
